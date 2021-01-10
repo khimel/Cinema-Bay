071e5dc0d5f01ee0bfc15ebb4a07c2d17f7b9bff
Mohammad Khimel <mkhimel@Mohammads-MBP.lan>
2021-01-10 06:47:18 +0200

    update movie page mhamad

--- APPLICATION-SOURCE-CODE/server.py
+++ APPLICATION-SOURCE-CODE/server.py
@@ -1,12 +1,7 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
-def index():
-
-    # here is a list that should be got from the db
-    movies = [
+movies = [
         {"name":"Jurassic Park", "poster":"https://i.pinimg.com/originals/e4/ca/5a/e4ca5a0c6a7b2dac40358e3f217174e0.jpg"},
         {"name":"The Godfather", "poster":"https://thelogocompany.net/wp-content/uploads/2017/05/The-Godfather-poster.jpg"},
         {"name":"Jaws", "poster":"https://www.companyfolders.com/blog/media/2017/07/jaws.jpg"},
@@ -16,10 +11,16 @@ def index():
         {"name":"Black Panther", "poster":"https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg"},
         {"name":"Joker", "poster":"https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1280x1280/products/89058/93685/Joker-2019-Final-Style-steps-Poster-buy-original-movie-posters-at-starstills__62518.1572351179.jpg?c=2?imbypass=on"},
         {"name":"Narnia", "poster":"https://www.arthipo.com/image/cache/catalog/poster/movie/1-758/pfilm289-fronty-wardrobe-narnia-narniya-poster-cizgi-film-1000x1000.jpg"}
-    ]
-    movies_names = [movies[i]["name"] for i in range(len(movies))] # for auto complete
-    movies_posters = [movies[i]["poster"] for i in range(len(movies))] # posters
-    return render_template('homepage.html',movies=movies, movies_names=movies_names, movies_posters=movies_posters)
+]
+movies_names = [movies[i]["name"] for i in range(len(movies))] # for auto complete
+movies_posters = [movies[i]["poster"] for i in range(len(movies))] # posters
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    # here is a list that should be got from the db
+    return render_template('home.html',movies=movies, movies_names=movies_names, movies_posters=movies_posters)
 
 @app.route('/search')
 def search_return_html():
@@ -38,7 +39,7 @@ def search_return_html():
     # if this movie is not from the auto complete, we redirect him for an "not found" html
         # return render_template('notFound.html', query=query)
     # with connector get to your mysql server and query the DB
-    return render_template('moviepage.html',query=query,context=context)
+    return render_template('movie.html',query=query,context=context,movies=movies, movies_names=movies_names, movies_posters=movies_posters)
 
 
 
